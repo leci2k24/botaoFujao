7c8e0e8342361d7f86814bb95328af626371a248
leci2k24 <162444665+leci2k24@users.noreply.github.com>
2024-07-10 09:53:55 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
+
+function alertSim() {
+    alert('Isso mesmo, precisa prestar atenção 😊')
+}
